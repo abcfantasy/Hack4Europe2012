1c466ecf7bf367698657e333d0231630fe6be832
Johan von Tangen Sivertsen <johanvts@gmail.com>
2012-06-13 20:29:29 +0200

    Working search for keyword and basic list

---
+++ Geopeana/Geopeana/EUPwebclient.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace Geopeana
+{
+    public class EUPwebclient
+    {
+        public XElement SearchResults;
+        public delegate void searchDone(XElement Result);
+        public event searchDone searchDoneEvent;
+
+        public void lookup(string city)
+        {
+
+            WebClient EuropeanaClient = new WebClient();
+
+            EuropeanaClient.DownloadStringCompleted += new DownloadStringCompletedEventHandler(EUP_DownloadStringCompleted);
+            EuropeanaClient.DownloadStringAsync(new Uri("http://api.europeana.eu/api/opensearch.rss?searchTerms=" + city + "&type:IMAGE&wskey=ZICPOGYUWT"));
+        }
+
+        void EUP_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            if (e.Error != null)
+                return;
+            XElement xmlItems = XElement.Parse(e.Result);
+
+            if (xmlItems.Element("channel").Element("item") == null)
+                return;
+
+            SearchResults = xmlItems;
+            if (searchDoneEvent != null) searchDoneEvent(SearchResults);
+
+        }
+    }
+}
--- Geopeana/Geopeana/MainPage.xaml.cs
+++ Geopeana/Geopeana/MainPage.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 using Microsoft.Phone.Controls;
 
 namespace Geopeana
@@ -19,6 +20,60 @@ namespace Geopeana
         public MainPage()
         {
             InitializeComponent();
+
+            // Set the data context of the listbox control to the sample data
+            DataContext = App.ViewModel;
+            this.Loaded += new RoutedEventHandler(MainPage_Loaded);
         }
+
+        // Handle selection changed on ListBox
+        private void MainListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // If selected index is -1 (no selection) do nothing
+            if (ResultsListBox.SelectedIndex == -1)
+                return;
+
+            // Navigate to the new page
+            NavigationService.Navigate(new Uri("/DetailsPage.xaml?selectedItem=" + ResultsListBox.SelectedIndex, UriKind.Relative));
+
+            // Reset selected index to -1 (no selection)
+            ResultsListBox.SelectedIndex = -1;
+        }
+
+        // Load data for the ViewModel Items
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (!App.ViewModel.IsDataLoaded)
+            {
+                App.ViewModel.LoadData();
+            }
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+        EUPwebclient EuropeanaAPI =new EUPwebclient();
+        EuropeanaAPI.searchDoneEvent+= new EUPwebclient.searchDone(EuropeanaAPI_searchDoneEvent);
+        EuropeanaAPI.lookup(CityBox.Text);
+        }
+        void EuropeanaAPI_searchDoneEvent(XElement SearchResults){
+
+        ResultsListBox.ItemsSource = from item in SearchResults.Element("channel").Descendants("item")
+                                        select new EUPItem
+                                        {
+                                            Thumbnail = item.Element("enclosure") != null ? item.Element("enclosure").Attribute("url").Value : "",
+                                            Link = item.Element("link").Value,
+                                            Title = item.Element("title").Value
+                                        };
+
+        }
+        
+
+    }
+
+    public class EUPItem
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string Thumbnail { get; set; }
     }
 }
