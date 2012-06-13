61eb2f20405162d120e34e821c5c6f12d5ecbf21
Andrew Borg Cardona <abcfantasy@gmail.com>
2012-06-13 17:10:34 +0200

    Storage class

---
+++ Geopeana/Geopeana/LocationData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.IsolatedStorage;
+
+namespace Geopeana
+{
+    /// <summary>
+    /// Usage
+    /// Create new instance of class.
+    /// To add elements: Data[key] = new SimpleCoordinates(long, lat);
+    /// To get element: Location = Data[key];
+    /// </summary>
+    public class LocationData
+    {
+        IsolatedStorageSettings storage;
+
+        // get virtual space for application
+        public LocationData()
+        {
+            storage = IsolatedStorageSettings.ApplicationSettings;
+        }
+
+        // indexer to get or add entries
+        public SimpleCoordinates this[string guid]
+        {
+            get
+            {
+                return (SimpleCoordinates)storage[guid];
+            }
+            set
+            {
+                storage.Add(guid, value);
+                storage.Save();
+            }
+        }
+    }
+}
